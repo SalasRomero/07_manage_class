2bf8f46dfa6a75f457978d9bed1b48fddab94417
David Salas <dasa.19983@gmail.com>
2024-11-05 23:36:10 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '07_manage_class',
+  webDir: 'www'
+};
+
+export default config;
